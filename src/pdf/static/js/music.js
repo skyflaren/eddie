e7c0721c5daeea7cee473a0cc4a0edf2bc716837
KennethRuan <kennethjruan@gmail.com>
2021-09-18 22:36:04 -0400

    added music player

--- src/pdf/static/js/music.js
+++ src/pdf/static/js/music.js
@@ -1,11 +1,168 @@
-$(document).ready(function() {
-	var audioElement = document.createElement("audio");
-	audioElement.src = "https://raw.githubusercontent.com/Metastruct/garrysmod-chatsounds/master/sound/chatsounds/autoadd/snoop_dogg/hold%20up%20wait.ogg";
-    $('#start').click(function(){
-        audioElement.play();
+$(document).ready(() => {
+    const musicContainer = document.getElementById('music-container');
+    const playBtn = document.getElementById('play');
+    const prevBtn = document.getElementById('prev');
+    const nextBtn = document.getElementById('next');
+
+    const audio = document.getElementById('audio');
+    const progress = document.getElementById('progress');
+    const progressContainer = document.getElementById('progress-container');
+    const title = document.getElementById('title');
+    const cover = document.getElementById('cover');
+    const currTime = document.querySelector('#currTime');
+    const durTime = document.querySelector('#durTime');
+
+    // Song titles
+    const songs = ['heart', 'mongoose'];
+
+    // Keep track of song
+    let songIndex = 0;
+
+    // Initially load song details into DOM
+
+    loadSong(songs[songIndex]);
+
+
+    // Update song details
+    function loadSong(song) {
+    // title.innerText = song;
+        audio.src =  GLOBAL_PATH + "static/media/" + song + ".mp3";
+        console.log(audio.src);
+        cover.src = GLOBAL_PATH + "static/img/" + songIndex + ".jpg";
+        console.log(cover.src);
+
+    }
+
+    // Play song
+    function playSong() {
+        musicContainer.classList.add('play');
+        playBtn.querySelector('i.fas').classList.remove('fa-play');
+        playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+        audio.play();
+    }
+
+    // Pause song
+    function pauseSong() {
+        musicContainer.classList.remove('play');
+        playBtn.querySelector('i.fas').classList.add('fa-play');
+        playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+        audio.pause();
+    }
+
+    function nextSong() {
+        songIndex++;
+        
+        if (songIndex > songs.length - 1) {
+            songIndex = 0;
+        }
+        
+        loadSong(songs[songIndex]);
+        
+        playSong();
+    }
+    
+
+    // Update progress bar
+    function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    }
+
+    // Set progress bar
+    function setProgress(e) {
+        const width = this.clientWidth;
+        const clickX = e.offsetX;
+        const duration = audio.duration;
+
+        audio.currentTime = (clickX / width) * duration;
+    }
+
+    //get duration & currentTime for Time of song
+    function DurTime (e) {
+        const {duration,currentTime} = e.srcElement;
+        var sec;
+        var sec_d;
+
+        // define minutes currentTime
+        let min = (currentTime==null)? 0:
+        Math.floor(currentTime/60);
+        min = min <10 ? '0'+min:min;
+
+        // define seconds currentTime
+        function get_sec (x) {
+            if(Math.floor(x) >= 60){
+                
+                for (var i = 1; i<=60; i++){
+                    if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+                        sec = Math.floor(x) - (60*i);
+                        sec = sec <10 ? '0'+sec:sec;
+                    }
+                }
+            }else{
+                sec = Math.floor(x);
+                sec = sec <10 ? '0'+sec:sec;
+            }
+        } 
+
+        get_sec (currentTime,sec);
+
+        // change currentTime DOM
+        currTime.innerHTML = min +':'+ sec;
+        // define minutes duration
+        let min_d = (isNaN(duration) === true)? '0':
+            Math.floor(duration/60);
+        min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+        function get_sec_d (x) {
+            if(Math.floor(x) >= 60){
+                
+                for (var i = 1; i<=60; i++){
+                    if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+                        sec_d = Math.floor(x) - (60*i);
+                        sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+                    }
+                }
+            }else{
+                sec_d = (isNaN(duration) === true)? '0':
+                Math.floor(x);
+                sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+            }
+        } 
+
+        // define seconds duration
+        
+        get_sec_d (duration);
+
+        // change duration DOM
+        durTime.innerHTML = min_d +':'+ sec_d;
+            
+    };
+
+    // Event listeners
+    playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
     });
 
-    // let url = chrome.runtime.getURL("https://raw.githubusercontent.com/Metastruct/garrysmod-chatsounds/master/sound/chatsounds/autoadd/snoop_dogg/hold%20up%20wait.ogg");
-    // let a = new Audio(url);
-    // a.play();
-}
+    // Time/song update
+    audio.addEventListener('timeupdate', updateProgress);
+
+    // Click on progress bar
+    progressContainer.addEventListener('click', setProgress);
+
+    // Song ends
+    audio.addEventListener('ended', nextSong);
+
+    // Time of song
+    audio.addEventListener('timeupdate',DurTime);
+
+});
